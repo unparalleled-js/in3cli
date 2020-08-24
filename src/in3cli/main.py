9cffa3169383004c266c033f76abb1365a85c961
unparalleled-js <yingthi@live.com>
2020-08-24 13:41:35 -0500

    List nodes

--- src/in3cli/main.py
+++ src/in3cli/main.py
@@ -5,25 +5,15 @@ from in3cli.eth import eth
 
 
 @click.command()
-def show_registry():
+def list_nodes():
     node_list = util.get_in3_client().refresh_node_list()
-    click.echo("\nIncubed Registry:")
-    click.echo("\ttotal servers: {}".format(node_list.totalServers))
-    click.echo("\tlast updated in block: {}".format(node_list.lastBlockNumber))
-    click.echo("\tregistry ID: {}".format(node_list.registryId))
-    click.echo("\tcontract address: {}".format(node_list.contract))
 
-
-@click.command()
-def show_nodes():
-    node_list = util.get_in3_client().refresh_node_list()
-    click.echo("\nNodes Registered:\n")
-    for node in node_list.nodes:
-        click.echo("\turl: {}".format(node.url))
-        click.echo("\tdeposit: {}".format(node.deposit))
-        click.echo("\tweight: {}".format(node.weight))
-        click.echo("\tregistered in block: {}".format(node.registerTime))
-
+    def gen():
+        for node in node_list.nodes:
+            yield "url: {}\ndeposit: {}\nweight: {}\nregistered in block: {}\n\n".format(
+                node.url, node.deposit, node.weight, node.registerTime
+            )
+    click.echo_via_pager(gen)
 
 
 @click.group()
@@ -32,5 +22,4 @@ def cli():
 
 
 cli.add_command(eth)
-cli.add_command(show_registry)
-cli.add_command(show_nodes)
+cli.add_command(list_nodes)
